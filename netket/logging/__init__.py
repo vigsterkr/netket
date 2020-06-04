1e54ef41618052aff2282eb7a100bcd66eec7fa6
Filippo Vicentini <filippovicentini@gmail.com>
2020-06-04 15:37:01 +0200

    sacred logging

--- netket/logging/__init__.py
+++ netket/logging/__init__.py
@@ -4,3 +4,12 @@ from ..utils import tensorboard_available
 
 if tensorboard_available:
     from ._tensorboard import TBLog
+
+
+try:
+    import sacred
+
+    sacred_available = True
+    from ._sacred import SacredLog
+except ImportError:
+    pass
